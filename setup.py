f58ce90ec57ee5f748156f9a4fdf942a7eecfce1
Reverchon <jean.louis.soleill@gmail.com>
2016-01-02 11:49:53 +0100

    J'ai mis à jour le set_up je vais voir s'il marche !

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ __author__ = 'samu'
 from setuptools import setup
 
 setup(
-    version='0.0',
+    version='1.0',
     name='sandbox',
     install_requires=['Pillow', 'scipy', 'numpy'],
     entry_points={
         'console_scripts': [
-            'sandbox_resize_dir = sandbox.images.resize:resize_dir',
+            'sandbox_resize_dir = sandbox.images.resize_v3:resize_dir',
         ]},
     packages=['sandbox', 'sandbox.images']
 )
